1d9080efd262fc468d1b7ee4cc51592d0b0eb0f9
Kirill Ki <kirillkalapov@gmail.com>
2021-12-28 17:32:53 +0100

    Initial Commit

---
+++ H4X0RNEWS/H4X0RNEWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RNEWSApp.swift
+//  H4X0RNEWS
+//
+//  Created by  Mr.Ki on 28.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RNEWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
